19ec08122834be553a37f89fac87eb5a934158f0
cyblanco <cblanco2012@gmail.com>
2015-12-12 12:53:45 -0500

    Setters & Getters for flight class

--- src/cis3270/user/EDITFlight.java
+++ src/cis3270/user/EDITFlight.java
@@ -14,7 +14,82 @@ public abstract class EDITFlight implements dbModify {
 		private int flightDeparture;
 		private int flightArrival;
 	
-		
+		public User() {
+			
 	}
-
+		
+		public User(int idFlight, int idPlane, String flightFrom, String flightTo, int flightDeparture, int flightArrival) {
+			
+			setIdFlight(idFlight);
+			setIdPlane(idPlane);
+			setFlightFrom(flightFrom);
+			setFlightTo(flightTo);
+			setFlightDeparture(flightDeparture);
+			serFlightArrival(flightArrival);
+			
+		}
+		
+		public void setIdFlight(int idFlight) {
+			
+			this.idFlight = idFlight;
+			
+		}
+		
+		public int getIdFlight() {
+			
+			return idFlight;
+			
+		}
+		
+		public void setIdPlane(int idPlane) {
+			
+			this.idPlane = idPlane;
+			
+		}
+		
+		public int getIdPlane() {
+			
+			return idPlane;
+		}
+		
+		public void setFlightFrom(String flightFrom) {
+			
+			this.flightFrom = flightFrom;
+		}
+		
+		public String getFlightFrom() {
+			
+			return flightFrom;
+		}
+		
+		public void setFlightTo(String flightTo) {
+			
+			this.flightTo = flightTo;
+		}
+		
+		public String getFlightTo() {
+			
+			return flightTo;
+		}
+		
+		public void setFlightDeparture(int flightDeparture) {
+			
+			this.flightDeparture = flightDeparture;
+		}
+		
+		public int getFlightDeparture() {
+			
+			return flightDeparture;
+		}
+		
+		public void setFlightArrival(int flightArrival) {
+			
+			this.flightArrival = flightArrival;
+		}
+		
+		public int getFlightArrival() {
+			
+			return flightArrival;
+		}
+		
 }
