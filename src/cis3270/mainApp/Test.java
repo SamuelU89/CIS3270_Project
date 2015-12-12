d84e405fc2c322eeed8964c28a8501d8ba0bae87
Samuel U <su1@student.gsu.edu>
2015-12-11 21:08:52 -0500

    Fix test
    testing if fix occured

--- src/cis3270/mainApp/Test.java
+++ src/cis3270/mainApp/Test.java
@@ -7,11 +7,9 @@ import cis3270.user.Customer;
 /* Use this Class to test your code */
 public class Test {
 	
-public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		Customer dan = new Customer("Sam", "U",  "19th street", "GA", 12343, "27ieor", 263099098, "doh?", "doh", "ddn", "u", 1);
-		
-		dan.add();
+		System.out.println("Fix test");
 		
 		
 	}
