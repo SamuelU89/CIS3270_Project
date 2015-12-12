958f8f956672979acfa346ee554c66135aabf4c7
cyblanco <cblanco2012@gmail.com>
2015-12-12 00:16:13 -0500

    First Version of Flight Class

---
+++ src/cis3270/user/EDITFlight.java
@@ -0,0 +1,14 @@
+package cis3270.user;
+
+import java.sql.*;
+import cis3270.database.dbModify;
+
+public abstract class EDITFlight implements dbModify {
+	
+	//Could not find original Flight Package we menioned before
+	
+	
+		
+	}
+
+}
