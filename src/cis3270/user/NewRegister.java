577240ea3913f01120f505f568721cb2a1e25750
cyblanco <cblanco2012@gmail.com>
2015-12-11 23:48:47 -0500

    First Name
    Textbox

--- src/cis3270/user/NewRegister.java
+++ src/cis3270/user/NewRegister.java
@@ -1,7 +1,22 @@
 package cis3270.user; 
+import javax.swing.*;
 
 public class NewRegister extends User {
-	
+	public static void main(String[] args) {
+		
+		// Creates a button that says "OK"
+		JButton jbtOK = new JButton("OK");
+		
+		// Creates a button that says "Cancel"
+		JButton jbtCancel = new JButton("Cancel");
+		
+		//Creates a label for first name
+		JLabel jlblFirstName = new JLabel("Enter first name: ");
+		
+		//Creates a text box for first name
+		JTextField jtfFirstName = new JTextField("Type First Name Here");
+		
+	}
 	
 }
 
