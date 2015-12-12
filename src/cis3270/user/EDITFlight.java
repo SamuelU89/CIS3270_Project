9a6a4eee337ca04055f070d507d21023fa708bf7
cyblanco <cblanco2012@gmail.com>
2015-12-12 12:33:13 -0500

    Set Privates

--- src/cis3270/user/EDITFlight.java
+++ src/cis3270/user/EDITFlight.java
@@ -6,7 +6,13 @@ import cis3270.database.dbModify;
 public abstract class EDITFlight implements dbModify {
 	
 	//Could not find original Flight Package we menioned before
-	
+		
+		private int idFlight;
+		private int idPlane;
+		private String flightFrom;
+		private String flightTo;
+		private int flightDeparture;
+		private int flightArrival;
 	
 		
 	}
