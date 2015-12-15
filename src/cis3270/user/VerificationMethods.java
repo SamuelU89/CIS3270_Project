e68569a02c5ac12fe83077863e5832a75f292789
cyblanco <cblanco2012@gmail.com>
2015-12-15 01:01:17 -0500

    Regular Expressions

--- src/cis3270/user/VerificationMethods.java
+++ src/cis3270/user/VerificationMethods.java
@@ -7,7 +7,7 @@ public class VerificationMethods {
 		Vars varsObject = new Vars();
 		String email = varsObject.getEmail();
 		String state = varsObject.getEmail();
-		int zip = varsObject.getEmail();
+		String zip = varsObject.getEmail();
 		
 		
 	}
@@ -24,23 +24,30 @@ public class VerificationMethods {
 		}
 	}
 	
-	public void state (String state) {
-		CharSequence[] stateab = new CharSequence ['ZZ', 'AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN',
-	            'IA','KS','KY','LA','ME','MD','MA','MI','MN', 'MS', 'MO','MT','NE','NV','NH',
-	            'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT',
-	            'VT','VA','WA','WV','WI','WY'];
+	public boolean state (String state) {
 		
-	   if(state.length() > 2) {
-			System.out.println("State is abbreviated");
+		if {
+		state.matches("AL | AK | AZ | AR |CA | CO |CT | DE | DC | FL "
+				+ "| GA | HI | ID | IL | IN |IA | KS | KY | LA "
+				+ "| ME | MD | MA | MI | MN | MS |MO | MT "
+				+ "| NE | NV | NH |NJ | NM | NY | NC | ND "
+				+ "| OH | OK | OR | PA | RI | SC | SD "
+				+ "| TN | TX | UT | VT | VA | WA | WV "
+				+ "| WI | WY ");
 		}
-		
-	   for(int i = 1; i < stateab.length; i++) {
-		   if ( state = stateab[i]) {
-			   System.out.println("State is one of the 50");
-		   }
-		   
-		   else
-			   System.out.println("State is not one of the 50");
+	            
+	   if(state.length() > 2) {
+			System.out.println("State is abbreviated");	
+		}
+	   
+	   else {
+		   return false;
 	   }
+	}
+	
+	public void zipcode(String zip) {
+		if(zip.matches("\\d{5}")) {
+			System.out.println("Zipcode is valid");
+		}
 	}
 }
