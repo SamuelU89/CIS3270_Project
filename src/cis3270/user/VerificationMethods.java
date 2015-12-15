8127e9ab8297f912d4000c24a828d28bcbf31240
cyblanco <cblanco2012@gmail.com>
2015-12-15 01:08:16 -0500

    edit to regular expressions

--- src/cis3270/user/VerificationMethods.java
+++ src/cis3270/user/VerificationMethods.java
@@ -14,7 +14,7 @@ public class VerificationMethods {
 	
 	public boolean email (String email) {
 		
-		if(email.contains(".com") && email.contains("@")) {
+		if(email.matches("..com") && email.matches(".@")) {
 			
 			return true;
 		}
@@ -26,24 +26,21 @@ public class VerificationMethods {
 	
 	public boolean state (String state) {
 		
-		if {
-		state.matches("AL | AK | AZ | AR |CA | CO |CT | DE | DC | FL "
+		if (state.matches("AL | AK | AZ | AR |CA | CO |CT | DE | DC | FL "
 				+ "| GA | HI | ID | IL | IN |IA | KS | KY | LA "
 				+ "| ME | MD | MA | MI | MN | MS |MO | MT "
 				+ "| NE | NV | NH |NJ | NM | NY | NC | ND "
 				+ "| OH | OK | OR | PA | RI | SC | SD "
 				+ "| TN | TX | UT | VT | VA | WA | WV "
-				+ "| WI | WY ");
+				+ "| WI | WY ") && state.length() > 2 ) {
+			return true;
+		}
+		
+		else 
+			return false;
+			
 		}
 	            
-	   if(state.length() > 2) {
-			System.out.println("State is abbreviated");	
-		}
-	   
-	   else {
-		   return false;
-	   }
-	}
 	
 	public void zipcode(String zip) {
 		if(zip.matches("\\d{5}")) {
